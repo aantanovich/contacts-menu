bd68ff27104e76ffd041d2ebd7f3f5c846f0d508
Anton <antanovich@gmail.com>
2020-07-06 12:22:55 -0400

    commit2

--- contacts.py
+++ contacts.py
@@ -1,15 +1,4 @@
-import time
-print('Hi, stranger!')
-# ask for their name
-print('What is your name?')
-theirName = input()
-time.sleep(2)
-print('It is good to meet you ' + theirName)
-print('The length of your name is: ')
-print(len(theirName))
-time.sleep(1)
-print('What is your age: ')
-theirAge = input()
-print('Your age will be ' + str(int(theirAge) + 1) + ' in a year.')
-time.sleep(2)
-print('Bye!')
+print("My name is ")
+for i in range(5):
+    print("Jimmy five times " + str(i))
+    
