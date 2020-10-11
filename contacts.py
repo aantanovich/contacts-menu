f088b1382a89065f7ac4a358779ac8f833bdf7c0
Anton <antanovich@gmail.com>
2020-10-11 10:06:00 -0400

    Oct 11

--- contacts.py
+++ contacts.py
@@ -1,6 +1,6 @@
 print("My name is ")
-for i in range(5):
-    print("Jimmy five times " + str(i))
-
-print("Fin")
+i = 0
+while i < 5:
+    print('Jimmy five times ('+ str(i) + ')')
+    i = i + 1
     
