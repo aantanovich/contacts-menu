7a847f91372b4705dc89d883048c42bb42bb3924
Anton <antanovich@gmail.com>
2020-10-11 20:36:41 -0400

    guessTheNumber

--- contacts.py
+++ contacts.py
@@ -1,7 +1,9 @@
-import sys
-while True:
-    print('Type exit to exit.')
-    response = input()
-    if response == 'exit':
-        sys.exit()
-    print('You typed ' + response + '.')
+#This is a guess the number game.
+import random
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20')
+
+#Ask the player to guess 6 times. 
+for guessTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
