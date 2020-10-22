edf249d0e3be23601ddcf3d1f35628ade07142c6
Anton <antanovich@gmail.com>
2020-10-22 10:37:06 -0400

    oct22.2

--- contacts.py
+++ contacts.py
@@ -1,3 +1,10 @@
 supplies = ['pens', 'cans', 'binders']
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+supplies += ['penciles']
+print(supplies[:])
+supplies.insert(2, 'fans')
+print(supplies[:])
+del supplies[-1]
+print(supplies[:])
